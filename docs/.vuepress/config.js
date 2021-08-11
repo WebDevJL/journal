0ad4d165cfeb1ac4705ba278db28440bd655938d
Jeremie Litzler <jeremie.litzler@conduent.com>
2021-08-11 08:04:08 +0200

    init project

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  lang: "en-US",
+  title: "Journal of Jérémie Litzler",
+  description:
+    "I want to share my notes, thoughts and experiences because I believe it is important",
+
+  //https://v2.vuepress.vuejs.org/reference/config.html#head
+  head: [
+    ["meta", { property: "og:title", content: "Journal of Jérémie Litzler" }],
+    ["meta", { property: "og:type", content: "website" }],
+    ["meta", { property: "og:url", content: "https://iamjeremie.me/" }],
+    [
+      "meta",
+      {
+        property: "og:description",
+        content: "Notes, thoughts and experiences of Jérémie.",
+      },
+    ],
+    ["meta", { property: "og:image", content: "image.png" }],
+    ["link", { rel: "icon", href: "/favicon.ico" }],
+    ["link", { rel: "icon", href: "/favicon.svg", type: "image/svg+xml" }],
+    ["link", { rel: "apple-touch-icon", href: "/apple-touch-icon.png" }],
+  ],
+  themeConfig: {
+    logo: "https://vuejs.org/images/logo.png",
+  },
+  bundler: "@vuepress/vite",
+  bundlerConfig: {
+    // vite bundler options
+  },
+};
