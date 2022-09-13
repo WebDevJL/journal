885bcda26fb8baa934f27ba2c42bf8e22c3c47d3
Jeremie Litzler <jeremie.litzler@gmail.com>
2022-09-13 08:13:27 +0200

    complete setup for vlog notes

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,4 @@
-const version = '1.0.168';
+const version = '1.0.169';
 const { path } = require('@vuepress/utils');
 const { vuePressThemeJeremieL } = require('./theme');
 const { viteBundler } = require('@vuepress/bundler-vite');
